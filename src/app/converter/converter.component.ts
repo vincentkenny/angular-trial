0a8b4cc1e0f864fc3b4d48796bc25879ed37af7b
unknown <vincent.kenny98@gmail.com>
2021-01-11 16:50:30 +0700

    finished

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExchangeRatesService } from '../services/exchange-rates.service';
 
 @Component({
   selector: 'app-converter',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./converter.component.css']
 })
 export class ConverterComponent implements OnInit {
+  
+  amount = 1;
+  from = 'CAD';
+  to = 'USD';
+  rates : {[key: string] : number};
+  convert(): number{
+    return this.amount * this.rates[this.to];
+  }
 
-  constructor() { }
+  loadRates(){
+    this.service.getRates(this.from).subscribe(res => this.rates = res.rates);
+  }
+
+  getAllCurrencies() : string[]{
+    return Object.keys(this.rates);
+  }
+
+  constructor(private service:ExchangeRatesService) {
+    
+    
+  }
 
   ngOnInit(): void {
+    this.loadRates();
   }
 
 }
